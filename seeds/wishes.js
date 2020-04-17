2ff6e0f957618996cd4ab58408f4dcabf748151a
KimHartley <kimhartley1986@gmail.com>
2020-04-17 12:18:46 +1200

    secondheroku

--- seeds/wishes.js
+++ seeds/wishes.js
@@ -2,5 +2,6 @@ exports.seed = function (knex, Promise){
     return knex('wishes').insert([
         {id:1, name:"Laniyuk", wishes:"endless supply of vegan fish sauce"},
         {id:2, name:"Kevin", wishes:"endless supply of soy sauce"}
+        {id:3, name:"Nini", wishes:"endless supply of top cuisine S&S"}
     ])
 }
