132c7cde056399667be841dcad5ef5e0fcfee232
Alice Jowsey <12345alcatraz@gmail.com>
2020-04-17 14:14:46 +1200

    tidy app.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -26,59 +26,24 @@ import { getDragonBallDetails } from '../api/get-api'
 // import characterPage from './Dragon.jsx'
 
 
-// class App extends React.Component {
-//     state = {
-//       dragon: []
-//     }
-//     componentDidMount() {
-//       this.update()
-//     }
+class App extends React.Component {
+    state = {
+      dragon: []
+    }
+    componentDidMount() {
+      this.update()
+    }
 
-//     update = () => {
-//       getDragonBallDetails()
-//       .then(dragon => {
-//         this.setState({
-//           dragon: dragon
-//         })
-//       })
-//     }
+    update = () => {
+      getDragonBallDetails()
+      .then(dragon => {
+        this.setState({
+          dragon: dragon
+        })
+      })
+    }
 
 
-// render() {
-//   // console.log(this.state.dragon)
-//   return (
-//     <Router>
-//       <header>
-//         <h1>Dragon Ball Z Characters</h1>
-//         <img src="" />
-//       </header>
-//           <ul>
-//               {this.state.dragon.map((dragon) => {
-//                 console.log(dragon)
-//                 return <p>
-//                         Kelly loves SCONES {dragon._id}
-//                       </p>
-//                   {/* return <Dragon key={dragon.id} data={dragon} /> */}
-//               })}
-//           </ul>
-//         <Route path='/description/:id' render={(props) => (
-//         <Description {...props} data={this.state.dragon} />)}/>
-//     </Router>
-//   )
-// }
-// }
-export default App
-
-// // species: "unknown"
-// // status: "Alive"
-// // originPlanet: "Unknown"
-// // gender: "Male"
-// // _id: "5c7c89fb12b25c00177aa150"
-// // name: "Frieza"
-// // series: "Z"
-// // created: "2019-03-04T02:14:19.836Z"
-// // url: "/api/character/Frieza"
-// // image: "../images/Frieza.jpg"":"../images/Goku.jpg","__v":0},
 render() {
   // console.log(this.state.dragon)
   return (
@@ -100,16 +65,3 @@ render() {
 }
 export default App
 
-//how do I call individual character from the array >
-//how do I call individual character from the array >
-
-// species: "unknown"
-// status: "Alive"
-// originPlanet: "Unknown"
-// gender: "Male"
-// _id: "5c7c89fb12b25c00177aa150"
-// name: "Frieza"
-// series: "Z"
-// created: "2019-03-04T02:14:19.836Z"
-// url: "/api/character/Frieza"
-// image: "../images/Frieza.jpg"":"../images/Goku.jpg","__v":0},
